4d04c38fe3e80427f8728bf56397d7aab63360f4
malik rasheed <codedev366@gmail.com>
2025-03-01 14:05:58 +0500

    Fix Bootstrap icons stylesheet link in product.html and remove unused tooltip initialization code from product.js

--- product.js
+++ product.js
@@ -102,12 +102,3 @@ if (productId) {
 } else {
   console.error("product ID not found in the url");
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  var tooltipTriggerList = [].slice.call(
-    document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  );
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
-  });
-});
