dab35431954929263c5a5580f8d6bda938a037bd
malik rasheed <codedev366@gmail.com>
2025-03-01 13:50:46 +0500

    Add tooltip initialization for Bootstrap tooltips on product page

--- product.js
+++ product.js
@@ -111,3 +111,12 @@ document.addEventListener("DOMContentLoaded", function () {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
